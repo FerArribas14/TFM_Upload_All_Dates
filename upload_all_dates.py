d3f10d5ec9601d3b3c460137787c9ddf346c1eba
Fernando Arribas <fernando.arribas.jara@gmail.com>
2020-09-02 00:36:53 +0200

    Cambios en script de carga

--- upload_all_dates.py
+++ upload_all_dates.py
@@ -7,8 +7,8 @@ end_date = datetime.date(2020, 9, 1)
 delta = datetime.timedelta(days=3)
 
 while start_date <= end_date:
-    print(start_date)
+    print("Upload date: " +start_date)
     r = requests.get('http://192.168.99.104:8085/date/'+ str(start_date))
-    print (r)
+    print ("Response STATUS: " + r)
     start_date += delta
     time.sleep(3)
